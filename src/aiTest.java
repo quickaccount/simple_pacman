678eef79d8b47522f29a1952329ea04904d8d914
RGingrich8 <richard.gingrich@ucalgary.ca>
2019-03-17 21:55:34 -0600

    Added a few more tests

--- src/aiTest.java
+++ src/aiTest.java
@@ -17,20 +17,58 @@ public class aiTest {
 
 
 	@Test
-	public void test_constructorRightBottom() {
-		AI testAi = new AI(ConstantVariables.WORLD_WIDTH - 15, ConstantVariables.WORLD_HEIGHT - 15); //Spawn inside wall on right/bottom side
+	public void test_constructor_left() {
+		AI testAi = new AI(0, 1); //Spawn inside wall on left side
 
-		assertEquals("Tried creating an AI at an invalid x position, too far right", 17, testAi.getXCoord());
-		assertEquals("Tried creating an AI at an invalid y position, too low down", 17, testAi.getYCoord());
+		assertEquals("Tried creating an AI at an invalid x position, too far left", 1, testAi.getXCoord());
 	}
 
 
 	@Test
-	public void test_constructorLeftTop() {
-		AI testAi = new AI(15, 15); //Spawn inside wall on left/top side
+	public void test_constructor_top() {
+		AI testAi = new AI(1, 0); //Spawn inside wall along top row
 
-		assertEquals("Tried creating an AI at an invalid x position, too far left", 17, testAi.getXCoord());
-		assertEquals("Tried creating an AI at an invalid y position, too high up", ConstantVariables.WORLD_HEIGHT - 17, testAi.getYCoord());
+		assertEquals("Tried creating an AI at an invalid y position, too high up", ConstantVariables.NUM_ROWS - 1, testAi.getYCoord());
 	}
 
+
+	@Test
+	public void test_constructor_right() {
+		AI testAi = new AI(ConstantVariables.NUM_COL, 1); //Spawn inside wall on right side
+
+		assertEquals("Tried creating an AI at an invalid x position, too far right", 1, testAi.getXCoord());
+	}
+
+
+	@Test
+	public void test_constructor_bottom() {
+		AI testAi = new AI(1, ConstantVariables.NUM_ROWS); //Spawn inside wall along top row
+
+		assertEquals("Tried creating an AI at an invalid y position, too far down", ConstantVariables.NUM_ROWS - 1, testAi.getYCoord());
+	}
+
+
+	@Test
+	public void test_constructor_proper() {
+		AI testAi = new AI(1, 1); //Acceptable spawn
+
+		assertEquals("Tried creating a valid AI", ">11", ">" + testAi.getXCoord() + testAi.getYCoord());
+	}
+
+	//Getters and Setters
+
+
+	@Test
+	public void test_setXDir_doubleMove() {
+		AI testAi = new AI(ConstantVariables.INITIAL_E_X, ConstantVariables.INITIAL_E_Y); //Create at proper position
+
+
+		assertEquals("Tested for a move of more than one tile", 1, testAi.getXCoord());
+	}
+
+
+	@Test
+	public void test_setYDir() {
+
+	}
 }
