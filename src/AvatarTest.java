323a1c4e968a5613bf641bfd72058e556c7d1517
RGingrich8 <richard.gingrich@ucalgary.ca>
2019-03-26 20:35:56 -0600

    Copy constructor + more tests added

--- src/AvatarTest.java
+++ src/AvatarTest.java
@@ -1,9 +1,8 @@
 /**
 * Test for AI class in Simple_Pacman
-cd Desktop/CPSC 233/simple_pacman-demo3/src
 javac AI.java
 javac -cp .;junit-4.12.jar;hamcrest-core-1.3.jar *.java
-java -cp .;junit-4.12.jar;hamcrest-core-1.3.jar org.junit.runner.JUnitCore aiTest
+java -cp .;junit-4.12.jar;hamcrest-core-1.3.jar org.junit.runner.JUnitCore AvatarTest
 * @author T10-G02
 */
 
@@ -13,47 +12,67 @@ import constants.ConstantVariables;
 import java.lang.Math;
 
 
-public class aiTest {
+public class AvatarTest {
 
 	//Constructor Tests
 
 	@Test
 	public void test_constructor_left() {
-		Avatar testAv = new AI(0, 1); //Spawn inside wall on left side
+		Avatar testAv = new Avatar(0, 1); //Spawn inside wall on left side
 
-		assertEquals("Tried creating an AI at an invalid x position, too far left", 1, testAv.getXCoord());
+		assertEquals("Tried creating the player at an invalid x position, too far left - [x-coord]", 1, testAv.getXCoord());
 	}
 
 
 	@Test
 	public void test_constructor_top() {
-		Avatar testAv = new AI(1, 0); //Spawn inside wall along top row
+		Avatar testAv = new Avatar(1, 0); //Spawn inside wall along top row
 
-		assertEquals("Tried creating an AI at an invalid y position, too high up", ConstantVariables.NUM_ROWS - 1, testAv.getYCoord());
+		assertEquals("Tried creating the player at an invalid y position, too high up - [y-coord]", ConstantVariables.NUM_ROWS - 1, testAv.getYCoord());
 	}
 
 
 	@Test
 	public void test_constructor_right() {
-		Avatar testAv = new AI(ConstantVariables.NUM_COL, 1); //Spawn inside wall on right side
+		Avatar testAv = new Avatar(ConstantVariables.NUM_COL, 1); //Spawn inside wall on right side
 
-		assertEquals("Tried creating an AI at an invalid x position, too far right", 1, testAv.getXCoord());
+		assertEquals("Tried creating the player at an invalid x position, too far right - [x-coord]", 1, testAv.getXCoord());
 	}
 
 
 	@Test
 	public void test_constructor_bottom() {
-		Avatar testAv = new AI(1, ConstantVariables.NUM_ROWS); //Spawn inside wall along top row
+		Avatar testAv = new Avatar(1, ConstantVariables.NUM_ROWS); //Spawn inside wall along top row
 
-		assertEquals("Tried creating an AI at an invalid y position, too far down", ConstantVariables.NUM_ROWS - 1, testAv.getYCoord());
+		assertEquals("Tried creating the player at an invalid y position, too far down - [y-coord]", ConstantVariables.NUM_ROWS - 1, testAv.getYCoord());
 	}
 
 
 	@Test
 	public void test_constructor_proper() {
-		Avatar testAv = new AI(4, 3); //Acceptable spawn
+		Avatar testAv = new Avatar(4, 3); //Acceptable spawn
 
-		assertEquals("Tried creating a valid AI, [x, y]", "11", "" + testAv.getXCoord() + testAv.getYCoord());
+		assertEquals("Tried creating a valid player, [x, y]", "11", "" + testAv.getXCoord() + testAv.getYCoord());
+	}
+
+
+	@Test
+	public void test_copyConstructor_xy() {
+		Avatar testAv = new Avatar(5, 1);
+		Avatar leak = new Avatar(testAv);
+
+		assertEquals("Copied Avatar(5, 1), checking [x, y] coordinates", "51", "" + leak.getXCoord() + leak.getYCoord());
+	}
+
+
+	@Test
+	public void test_copyConstructor_score() {
+		Avatar testAv = new Avatar(1, 1);
+		testAv.addScore(); 
+		testAv.addScore();
+		Avatar leak = new Avatar(testAv);
+
+		assertEquals("Copied Avatar, score is 2", 2, leak.getScore());
 	}
 
 
