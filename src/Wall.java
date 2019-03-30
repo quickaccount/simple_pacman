39aec7975cfb4b4e8c23646b559160acd6669131
Sydney Kwok <sydney.kwok@ucalgary.ca>
2019-03-30 14:42:40 -0600

    additional commenting

--- src/Wall.java
+++ src/Wall.java
@@ -1,15 +1,19 @@
-//import javafx.scene.shape.*;
-import constants.ConstantVariables;
+//import constants.ConstantVariables;
+
+/**
+ * The Wall class extends the abstract class Item. A Wall object represents a
+ * wall at specific (x,y) coordinates in the Pac-Man maze.
+ */
 
 public class Wall extends Item {
 
-
-    /**
-    * Creates a wall at the specified location
-    * @param x the x position
-    * @param y the y position
-    */
-    public Wall (int x, int y) {
-        super(x, y);
-    }
+	/**
+	 * Creates a wall at the specified location
+	 * 
+	 * @param x the x position
+	 * @param y the y position
+	 */
+	public Wall(int x, int y) {
+		super(x, y);
+	}
 }
